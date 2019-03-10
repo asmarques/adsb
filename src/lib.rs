0b31aed1295e05277de866313741eb228a238a62
António Marques <me@antoniomarques.eu>
2019-03-10 11:34:07 +0000

    Implement parsing for airborne position messages

--- src/lib.rs
+++ src/lib.rs
@@ -13,6 +13,12 @@ pub struct ParserError();
 #[derive(Debug, PartialEq)]
 pub struct ICAOAddress(u8, u8, u8);
 
+#[derive(Debug, PartialEq)]
+pub enum CPRFrame {
+    Odd,
+    Even,
+}
+
 #[derive(Debug)]
 pub struct Message {
     pub downlink_format: u8,
@@ -35,6 +41,12 @@ pub enum ADSBMessageKind {
         emitter_category: u8,
         callsign: String,
     },
+    AirbornePosition {
+        altitude: u16,
+        cpr_frame: CPRFrame,
+        cpr_latitude: u32,
+        cpr_longitude: u32,
+    },
 }
 
 fn decode_callsign(encoded: Vec<u8>) -> String {
@@ -46,19 +58,59 @@ fn decode_callsign(encoded: Vec<u8>) -> String {
 
 named!(parse_adsb_message_kind<&[u8], ADSBMessageKind>,
     alt!(
-        parse_aircraft_identification
+        parse_aircraft_identification |
+        parse_airborne_position
     )
 );
 
 named!(parse_aircraft_identification<&[u8], ADSBMessageKind>,
     bits!(
         do_parse!(
-            verify!(take_bits!(u8, 5), |v| v >= 1 && v <= 4) >>
+            verify!(take_bits!(u8, 5), |tc| tc >= 1 && tc <= 4) >>
             emitter_category: take_bits!(u8, 3) >>
             callsign: map!(many_m_n!(8, 8, take_bits!(u8, 6)), decode_callsign) >>
             (ADSBMessageKind::AircraftIdentification {
                 emitter_category,
                 callsign,
+            })
+        )
+    )
+);
+
+named!(parse_altitude<(&[u8], usize), u16>,
+    do_parse!(
+        l: take_bits!(u16, 7) >>
+        q: alt!(
+            tag_bits!(u8, 1, 0b0) => {|_| 100 } |
+            tag_bits!(u8, 1, 0b1) => {|_| 25 }
+        ) >>
+        r: take_bits!(u16, 4) >>
+        ((l.rotate_left(4) + r) * q - 1000)
+    )
+);
+
+named!(parse_cpr_frame<(&[u8], usize), CPRFrame>,
+    alt!(
+        tag_bits!(u8, 1, 0b0) => {|_| CPRFrame::Even } |
+        tag_bits!(u8, 1, 0b1) => {|_| CPRFrame::Odd }
+    )
+);
+
+named!(parse_airborne_position<&[u8], ADSBMessageKind>,
+    bits!(
+        do_parse!(
+            verify!(take_bits!(u8, 5), |tc| tc >= 9 && tc <= 18) >>
+            take_bits!(u8, 3) >>
+            altitude: parse_altitude >>
+            take_bits!(u8, 1) >>
+            cpr_frame: parse_cpr_frame  >>
+            cpr_latitude: take_bits!(u32, 17) >>
+            cpr_longitude: take_bits!(u32, 17) >>
+            (ADSBMessageKind::AirbornePosition {
+                altitude,
+                cpr_frame,
+                cpr_latitude,
+                cpr_longitude
             })
         )
     )
@@ -114,6 +166,7 @@ pub fn parse_message(message: &[u8]) -> Result<Message, Error> {
 #[cfg(test)]
 mod tests {
     use super::*;
+    const CAPABILITY: u8 = 5;
 
     #[test]
     fn parse_aircraft_identification_message() {
@@ -123,12 +176,54 @@ mod tests {
         assert_eq!(
             m.kind,
             MessageKind::ADSBMessage {
-                capability: 5,
+                capability: CAPABILITY,
                 icao_address: ICAOAddress(0x48, 0x40, 0xD6),
                 type_code: 4,
                 kind: ADSBMessageKind::AircraftIdentification {
                     emitter_category: 0,
                     callsign: "KLM1023 ".to_string(),
+                }
+            }
+        );
+    }
+
+    #[test]
+    fn parse_airborne_position_even_message() {
+        let r = b"\x8D\x40\x62\x1D\x58\xC3\x82\xD6\x90\xC8\xAC\x28\x63\xA7";
+        let m = parse_message(r).unwrap();
+        assert_eq!(m.downlink_format, 17);
+        assert_eq!(
+            m.kind,
+            MessageKind::ADSBMessage {
+                capability: CAPABILITY,
+                icao_address: ICAOAddress(0x40, 0x62, 0x1D),
+                type_code: 11,
+                kind: ADSBMessageKind::AirbornePosition {
+                    altitude: 38000,
+                    cpr_frame: CPRFrame::Even,
+                    cpr_latitude: 93000,
+                    cpr_longitude: 51372,
+                }
+            }
+        );
+    }
+
+    #[test]
+    fn parse_airborne_position_odd_message() {
+        let r = b"\x8D\x40\x62\x1D\x58\xC3\x86\x43\x5C\xC4\x12\x69\x2A\xD6";
+        let m = parse_message(r).unwrap();
+        assert_eq!(m.downlink_format, 17);
+        assert_eq!(
+            m.kind,
+            MessageKind::ADSBMessage {
+                capability: CAPABILITY,
+                icao_address: ICAOAddress(0x40, 0x62, 0x1D),
+                type_code: 11,
+                kind: ADSBMessageKind::AirbornePosition {
+                    altitude: 38000,
+                    cpr_frame: CPRFrame::Odd,
+                    cpr_latitude: 74158,
+                    cpr_longitude: 50194,
                 }
             }
         );
